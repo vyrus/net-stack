5205112b71e85c7532fd54f5a86f5f6e53c8aa61
Vyrus <piqlet@gmail.com>
2010-02-14 05:19:28 +0300

    [*] Дополнен интерфейс искры.

--- lib/IO/Stream/Spark/Interface.php
+++ lib/IO/Stream/Spark/Interface.php
@@ -5,6 +5,14 @@
     */
     interface IO_Stream_Spark_Interface {
         /**
+        * Установка опций искры.
+        * 
+        * @param  array|Options $options
+        * @return IO_Stream_Spark_Interface Fluent interface.
+        */
+        public function setOptions($options = array());
+        
+        /**
         * Открытие потока.
         * 
         * @return boolean
